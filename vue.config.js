52ebbf1be2df90aa2784cb31314cc32a2bada4db
Foxyg3n <piotrek.suchecki@vp.pl>
2024-06-14 11:15:48 +0200

    Added SEO meta tags, changed icon and project name

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
@@ -13,5 +14,14 @@ module.exports = defineConfig({
   },
   css: {
     sourceMap: true
+  },
+  chainWebpack: config => {
+    config.plugin('html').tap(args => {
+      args[0].title = 'Dekar\'s Dojo';
+      args[0].description = 'It\'s a site displaying Dekar\'s twitch games along with information about runes/build designed for learning how to counter certain champions as well as how to play Rengar.';
+      args[0].keywords = 'league, of, legends, lol, dekar, dekar173, learn, practice, rengar, jungle, top, lane';
+      args[0].author = 'Foxyg3n';
+      return args;
+    });
   }
 })
