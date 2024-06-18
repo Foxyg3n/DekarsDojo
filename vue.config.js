2630a9c76bb12fadbfcfe1c0c63de25a7e1f98b6
Foxyg3n <piotrek.suchecki@vp.pl>
2024-06-18 18:53:25 +0200

    Removed keywords that may brake Legal Jibber Jabber terms

--- vue.config.js
+++ vue.config.js
@@ -19,7 +19,7 @@ module.exports = defineConfig({
     config.plugin('html').tap(args => {
       args[0].title = 'Dekar\'s Dojo';
       args[0].description = 'It\'s a site displaying Dekar\'s twitch games along with information about runes/build designed for learning how to counter certain champions as well as how to play Rengar.';
-      args[0].keywords = 'league, of, legends, lol, dekar, dekar173, learn, practice, rengar, jungle, top, lane';
+      args[0].keywords = 'lol, dekar, dekar173, learn, practice, rengar, jungle, top, lane';
       args[0].author = 'Foxyg3n';
       return args;
     });
